98a4547e8fbcd15baa3deb7f3dc0d4e63a82c04b
Jitesh Shewale <jiteshshewale40@gmail.com>
2024-01-19 13:48:58 +0530

    created HelloWorldAPivew

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
 # Create your models here.
+class UserProfileManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError("Email must be provided")
+
+        email = self.normalize_email(email)  #converts mail to std format
+        user = self.model(email=email, name=name)
+
+        user.set_password(password) #password encrypted automatically
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(email, name, password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """setting up user model"""
+    email = models.EmailField(max_length=200, unique=True)
+    name = models.CharField(max_length=500)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+    def get_full_name(self):
+        return self.name
+
+    def get_short_name(self):
+        return self.name
+
+    def __str__(self):
+        return self.email
